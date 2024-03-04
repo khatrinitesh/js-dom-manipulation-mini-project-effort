a9ad8198575b425f8baa414c67b1971033aa519b
nitesh <nitesh.khatri88@gmail.com>
2024-03-04 18:33:59 +0530

    updated

--- custom.js
+++ custom.js
@@ -20927,3 +20927,40 @@ const items = ['item1', 'item2', 'item3']
 //     console.log('d')
 // }
 
+// console.log(2+2)
+// console.log("2"+"2")
+// console.log(2+2-2)
+// console.log("2"+"2"-"2")
+
+// const sortedData = (arr1, arr2) => {
+//     let i = 1;
+//     let j = 1;
+//     let array1 = arr1[0];
+//     let array2 = arr2[0];
+//     let mergedArray = [];
+//     while (array1 || array2) {
+//     if (array2 === undefined || array1 < array2) {
+//     mergedArray.push(array1);
+//     array1 = arr1[i];
+//     i++;
+//     } else {
+//     mergedArray.push(array2);
+//     array2 = arr2[j];
+//     j++;
+//     }
+//     }
+//     console.log(mergedArray);
+//    };
+   
+//    sortedData([1, 3, 4, 5], [2, 6, 8, 9]);
+
+// let name1 = {
+//     name1:'nitesh',
+//     age1:34
+// }
+// let name2 = {
+//     name2:'sameet',
+//     age2:41
+// }
+// let result = Object.assign({},name1,name2)
+// console.log(result)
